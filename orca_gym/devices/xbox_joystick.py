51a4a6b9af8cffcfe1f089707feaf35c2ed1ed24
superfhwl <superfhwl@gmail.com>
2024-08-08 13:02:48 +0800

    + game pad control support linux platform

--- orca_gym/devices/xbox_joystick.py
+++ orca_gym/devices/xbox_joystick.py
@@ -1,22 +1,30 @@
 import pygame
-
+import sys
 
 class XboxJoystick:
     def __init__(self):
-        # 初始化 Pygame
+        # Initialize Pygame
         pygame.init()
         
-        # 初始化 Joystick
+        # Initialize Joystick
         pygame.joystick.init()
 
         if pygame.joystick.get_count() == 0:
-            raise Exception("没有检测到手柄")
+            raise Exception("No joystick detected")
         else:
             self.joystick = pygame.joystick.Joystick(0)
             self.joystick.init()
-            print("检测到手柄：", self.joystick.get_name())
+            print("Joystick detected:", self.joystick.get_name())
 
-        # 初始化手柄状态
+        # Check the operating system
+        if sys.platform.startswith('win'):
+            self.platform = 'windows'
+        elif sys.platform.startswith('linux'):
+            self.platform = 'linux'
+        else:
+            raise Exception("Unsupported platform")
+
+        # Initialize joystick state
         self.joystick_state = {
             "buttons": {
                 "A": 0, "B": 0, "X": 0, "Y": 0,
@@ -34,27 +42,37 @@ class XboxJoystick:
         }
 
         self.button_map = {
-            0: "A", 1: "B", 2: "X", 3: "Y",
-            4: "LB", 5: "RB", 6: "Back", 7: "Start",
-            8: "LeftStick", 9: "RightStick"
+            "windows":{
+                0: "A", 1: "B", 2: "X", 3: "Y",
+                4: "LB", 5: "RB", 6: "Back", 7: "Start",
+                8: "LeftStick", 9: "RightStick"},
+            "linux":{            
+                0: "A", 1: "B", 2: "X", 3: "Y",
+                4: "LB", 5: "RB", 6: "Back", 7: "Start",
+                8: "LeftStick", 9: "RightStick"}
         }
 
         self.axis_map = {
-            0: "LeftStickX", 1: "LeftStickY",
-            2: "RightStickX", 3: "RightStickY",
-            4: "LT", 5: "RT"
+            "windows": {
+                0: "LeftStickX", 1: "LeftStickY",
+                2: "RightStickX", 3: "RightStickY",
+                4: "LT", 5: "RT"},
+            "linux": {
+                0: "LeftStickX", 1: "LeftStickY",
+                3: "RightStickX", 4: "RightStickY",
+                2: "LT", 5: "RT"}
         }
         
     def update(self):
         for event in pygame.event.get():
             # print("event: ", event)
             if event.type == pygame.JOYBUTTONDOWN:
-                self.joystick_state["buttons"][self.button_map[event.button]] = 1
+                self.joystick_state["buttons"][self.button_map[self.platform][event.button]] = 1
             if event.type == pygame.JOYBUTTONUP:
-                self.joystick_state["buttons"][self.button_map[event.button]] = 0
+                self.joystick_state["buttons"][self.button_map[self.platform][event.button]] = 0
             if event.type == pygame.JOYAXISMOTION:
-                if event.axis in self.axis_map:
-                    self.joystick_state["axes"][self.axis_map[event.axis]] = event.value
+                if event.axis in self.axis_map[self.platform]:
+                    self.joystick_state["axes"][self.axis_map[self.platform][event.axis]] = event.value
             if event.type == pygame.JOYHATMOTION:
                 self.joystick_state["hats"]["DPad"] = event.value
 
@@ -63,4 +81,4 @@ class XboxJoystick:
 
     def close(self):
         pygame.quit()
-        print("手柄已关闭")
+        print("Joystick closed")
