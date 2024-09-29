! Fix openloong vr control rotation bug.

envs/robosuite/Openloong_arm_vr_env.py
@@ -63,6 +63,11 @@ class OpenloongArmEnv(MujocoRobotEnv):
         # 9 arm joints and 6 free joints
         self.nv = self.model.nv
 
+        self._base_body_name = [self.body("base_link")]
+        self._base_body_xpos, _, self._base_body_xquat = self.get_body_xpos_xmat_xquat(self._base_body_name)
+        print("base_body_xpos: ", self._base_body_xpos)
+        print("base_body_xquat: ", self._base_body_xquat)
+
         # index used to distinguish arm and gripper joints
         self._r_arm_joint_names = [self.joint("J_arm_r_01"), self.joint("J_arm_r_02"), 
                                  self.joint("J_arm_r_03"), self.joint("J_arm_r_04"), 
@@ -91,6 +96,7 @@ class OpenloongArmEnv(MujocoRobotEnv):
                                 self.actuator("M_arm_l_05"),self.actuator("M_arm_l_06"),self.actuator("M_arm_l_07")]
         self._l_arm_actuator_id = [self.model.actuator_name2id(actuator_name) for actuator_name in self._l_arm_moto_names]
         self._l_neutral_joint_values = np.array([-0.905, 0.735, 2.733, 1.405, 1.191, 0.012, 0.517])
+        # self._l_neutral_joint_values = np.zeros(7)
 
         print("arm_actuator_id: ", self._l_arm_actuator_id)
         self._l_hand_moto_names = [self.actuator("M_zbll_J1"), self.actuator("M_zbll_J2"), self.actuator("M_zbll_J3")
@@ -316,6 +322,7 @@ class OpenloongArmEnv(MujocoRobotEnv):
             self.set_grasp_mocap(mocap_l_xpos, mocap_l_xquat)
             self.set_grasp_mocap_r(mocap_r_xpos, mocap_r_xquat)
             self._process_pico_joystick_operation()
+            # print("base_body_euler: ", self._base_body_euler / np.pi * 180)
         else:
             return
 
@@ -356,6 +363,7 @@ class OpenloongArmEnv(MujocoRobotEnv):
 
     def _processe_pico_joystick_move(self):
         if self._pico_joystick.is_reset_pos():
+            self._pico_joystick.set_reset_pos(False)
             self._set_init_state()
 
         transform_list = self._pico_joystick.get_transform_list()
@@ -363,28 +371,43 @@ class OpenloongArmEnv(MujocoRobotEnv):
             return self._initial_grasp_site_xpos, self._initial_grasp_site_xquat, self._initial_grasp_site_xpos_r, self._initial_grasp_site_xquat_r
 
         left_relative_position, left_relative_rotation = self._pico_joystick.get_left_relative_move(transform_list)
-        right_relative_postion, right_relative_rotation = self._pico_joystick.get_right_relative_move(transform_list)
-        # left_relative_position, left_relative_rotation = self._pico_joystick.get_motion_trackers_relative_move(transform)[0]
-        # right_relative_postion, right_relative_rotation = self._pico_joystick.get_motion_trackers_relative_move(transform)[1]
+        right_relative_position, right_relative_rotation = self._pico_joystick.get_right_relative_move(transform_list)
 
-        # ee_l_xpos, ee_l_xmat = self.get_ee_xform()
-        # ee_r_xpos, ee_r_xmat = self.get_ee_r_xform()
+        # left_relative_position_org, left_relative_rotation_org = self._pico_joystick.get_left_relative_move_org(transform_list)
+        # right_relative_position_org, right_relative_rotation_org = self._pico_joystick.get_right_relative_move_org(transform_list)
 
-        mocap_l_xpos = self._initial_grasp_site_xpos + left_relative_position
-        mocap_r_xpos = self._initial_grasp_site_xpos_r + right_relative_postion
+        # print("left_relative_position: ", left_relative_position)
+        # print("left_relative_rotation: ", rotations.quat2euler(left_relative_rotation) * 180 / np.pi)
+        # print("right_relative_position: ", right_relative_position)
+        # print("right_relative_rotation: ", R.from_quat(right_relative_rotation, scalar_first=True).as_euler('xzy', degrees=True))
+        # print("right_relative_rotation_org: ", R.from_quat(right_relative_rotation_org, scalar_first=True).as_euler('xzy', degrees=True))
 
+        # def decompose(quat):
+        #     v = R.from_quat(quat, scalar_first=True).as_rotvec(degrees=True)
+        #     l = np.linalg.norm(v)
+        #     v = v / l
+        #     return [f'{v[0]:>12.6f} {v[1]:>12.6f} {v[2]:>12.6f}', l]
+        
 
+            # v = R.from_quat(quat, scalar_first=True).as_euler('zxy', degrees=True)
+            # return f'{v[0]:>12.6f} {v[1]:>12.6f} {v[2]:>12.6f}'
 
-        # mocap_l_xquat = (R.from_euler('xyz', self._initial_grasp_site_euler) * left_relative_rotation).as_quat()
-        left_relative_xquat = (R.from_euler('xyz', [0, 0, 0]) * left_relative_rotation).as_quat()
-        mocap_l_xquat = (R.from_quat(self._initial_grasp_site_xquat) * R.from_quat(left_relative_xquat)).as_quat()
-        # mocap_r_xquat = (R.from_euler('xyz', self._initial_grasp_site_euler_r) * right_relative_rotation).as_quat()
-        right_relative_xquat = (R.from_euler('xyz', [0, 0, 0]) * right_relative_rotation).as_quat()
-        mocap_r_xquat = (R.from_quat(self._initial_grasp_site_xquat_r) * R.from_quat(right_relative_xquat)).as_quat()
+        # print("rotation_org: ", decompose(right_relative_rotation_org))
+        # print("rotation_mujo:", decompose(right_relative_rotation))
 
+        mocap_l_xpos = self._initial_grasp_site_xpos + rotations.quat_rot_vec(self._base_body_xquat, left_relative_position)
+        mocap_r_xpos = self._initial_grasp_site_xpos_r + rotations.quat_rot_vec(self._base_body_xquat, right_relative_position)
+
+        mocap_l_xquat = rotations.quat_mul(self._initial_grasp_site_xquat, left_relative_rotation)
+        # mocap_r_xquat = rotations.quat_mul(self._initial_grasp_site_xquat_r, right_relative_rotation)
+        mocap_r_xquat = (R.from_quat(self._initial_grasp_site_xquat_r, scalar_first=True) * 
+                         R.from_quat(right_relative_rotation, scalar_first=True)).as_quat(scalar_first=True, canonical=True)
+        
+   
 
         return mocap_l_xpos, mocap_l_xquat, mocap_r_xpos, mocap_r_xquat
-    
+
+
     def _process_pico_joystick_operation(self):
         joystick_state = self._pico_joystick.get_key_state()
         if joystick_state is None:
@@ -435,19 +458,19 @@ class OpenloongArmEnv(MujocoRobotEnv):
         self.mj_forward()
 
         print("""
-              To use VR controllers, please press both B and Y buttons to establish connection.
-              And then press A and X buttons to reset the position.
+              To use VR controllers, please press both B and Y buttons to connect / disconnect to the simulator.
+              And then press A and X buttons to reset the robot's hands to the initial position.
               """)        
         return True
 
     # custom methods
     # -----------------------------
     def set_grasp_mocap(self, position, orientation) -> None:
-        mocap_pos_and_quat_dict = {self.mocap("rm65b_mocap"): {'pos': position, 'quat': orientation}}
+        mocap_pos_and_quat_dict = {self.mocap("leftHandMocap"): {'pos': position, 'quat': orientation}}
         self.set_mocap_pos_and_quat(mocap_pos_and_quat_dict)
 
     def set_grasp_mocap_r(self, position, orientation) -> None:
-        mocap_pos_and_quat_dict = {self.mocap("rm65b_mocap_r"): {'pos': position, 'quat': orientation}}
+        mocap_pos_and_quat_dict = {self.mocap("rightHandMocap"): {'pos': position, 'quat': orientation}}
         # print("Set grasp mocap: ", position, orientation)
         self.set_mocap_pos_and_quat(mocap_pos_and_quat_dict)
 

orca_gym/devices/pico_joytsick.py
@@ -4,9 +4,137 @@ import threading
 import numpy as np
 from websockets.asyncio.server import serve
 import copy
-from scipy.spatial.transform import Rotation as R
 import threading
+from scipy.spatial.transform import Rotation as R
 
+   
+# def quaternion_to_matrix(q):
+#     # 将四元数转换为旋转矩阵
+#     R_matrix = R.from_quat(q).as_matrix()
+#     return R_matrix
+
+# def apply_coordinate_transform(R_matrix):
+#     # 定义坐标轴变换矩阵（交换 Y 和 Z 轴）
+#     T = np.array([[1, 0, 0],
+#                 [0, 0, 1],
+#                 [0, 1, 0]])
+#     # 应用坐标轴变换
+#     R_transformed = T @ R_matrix @ T.T
+#     return R_transformed
+
+# def correct_rotation_matrix(R_matrix):
+#     # 使用 SVD 分解
+#     U, _, Vt = np.linalg.svd(R_matrix)
+#     # 重新计算旋转矩阵
+#     R_corrected = U @ Vt
+#     # 确保行列式为 1
+#     if np.linalg.det(R_corrected) < 0:
+#         U[:, -1] *= -1
+#         R_corrected = U @ Vt
+#     return R_corrected
+
+# def matrix_to_quaternion(R_matrix):
+#     # 将旋转矩阵转换回四元数
+#     R_obj = R.from_matrix(R_matrix)
+#     q = R_obj.as_quat()
+#     return q
+
+# def transform_quaternion(q):
+#     # 手系转换：对 Y 和 Z 分量取反（从左手系到右手系）
+#     q_lhs = np.array([q[0], q[1], q[2], q[3]])
+
+#     # 将四元数转换为旋转矩阵
+#     R_matrix = quaternion_to_matrix(q_lhs)
+
+#     # 应用坐标轴变换
+#     R_transformed = apply_coordinate_transform(R_matrix)
+
+#     # 修正旋转矩阵
+#     R_transformed = correct_rotation_matrix(R_transformed)
+
+#     # 将旋转矩阵转换回四元数
+#     q_transformed = matrix_to_quaternion(R_transformed)
+
+#     return q_transformed
+
+
+# def transform_position(p):
+#     # 手系转换：对 Z 轴取反
+#     p_rhs = np.array([p[0], p[1], -p[2]])
+#     # 坐标轴互换：交换 Y 和 Z 轴
+#     p_transformed = np.array([p_rhs[0], p_rhs[2], p_rhs[1]])
+#     return p_transformed
+
+# def transform_quaternion(q):
+#     """
+#     将四元数从 Unity 左手系（Y-up，Z-forward）转换到目标右手系（Z-up，Y-forward）
+#     """
+#     # 将四元数转换为旋转矩阵
+#     R_matrix = quaternion_to_matrix(q)
+
+#     # 应用坐标系转换
+#     R_transformed = transform_rotation_matrix(R_matrix)
+
+#     # 将旋转矩阵转换回四元数
+#     q_transformed = matrix_to_quaternion(R_transformed)
+#     return q_transformed
+
+# def quaternion_to_matrix(q):
+#     R_matrix = R.from_quat(q).as_matrix()
+#     return R_matrix
+
+# def transform_rotation_matrix(R_matrix):
+#     T = np.array([
+#         [1, 0, 0],
+#         [0, 0, 1],
+#         [0, -1, 0]
+#     ])
+#     T_inv = T.T
+#     R_transformed = T @ R_matrix @ T_inv
+#     return R_transformed
+
+# def matrix_to_quaternion(R_matrix):
+#     q = R.from_matrix(R_matrix).as_quat()
+#     return q
+
+# def transform_position(p):
+#     """
+#     将位置向量从 Unity 左手系（Y-up，Z-forward）转换到目标右手系（Z-up，Y-forward）
+#     """
+#     T = np.array([
+#         [1, 0, 0],
+#         [0, 0, 1],
+#         [0, -1, 0]
+#     ])
+#     p_transformed = T @ p
+#     return p_transformed
+
+def transform_position_to_mujoco(p_unity):
+    x_mujoco = p_unity[2]
+    y_mujoco = -p_unity[0]
+    z_mujoco = p_unity[1]
+    return np.array([x_mujoco, y_mujoco, z_mujoco])
+
+def transform_quaternion_to_mujoco(q_unity):
+    #q_fixed = R.from_quat(q_unity, scalar_first=False).as_quat(canonical=True)
+    R_unity = R.from_quat(q_unity, scalar_first=False).as_matrix()
+    T = np.array([
+        [-1, 0, 0],
+        [0, 0, 1],
+        [0, -1, 0]
+    ])
+    R_mujoco = T @ R_unity @ T.T
+    # R_mujoco = T @ R_unity 
+    # R_mujoco = R_unity @ T
+    # R_mujoco = R_unity 
+    # q_mujoco = R.from_matrix(R_mujoco).as_quat(canonical=True)
+
+    v = R.from_matrix(R_mujoco).as_rotvec()
+
+    return R.from_rotvec(v).as_quat(canonical=True)
+    # return R.from_quat(q_unity).as_quat(canonical=True)
+
+    # return [ q_unity[0], q_unity[2], q_unity[1], -q_unity[3]]
 
 class PicoJoystick:
     def __init__(self):
@@ -14,7 +142,6 @@ class PicoJoystick:
         self.running = True
         self.thread = threading.Thread(target=self._loop)
         self.thread.start()
-        self.first_transform = None
         self.current_transform = None
         self.current_key_state = None
         self.reset_pos = False
@@ -36,7 +163,6 @@ class PicoJoystick:
                     self.current_transform = self.extact_all_transform(json.loads(message))
                     self.current_key_state = self.extact_key_state(json.loads(message))
                     if is_first_message:
-                        self.first_transform = copy.deepcopy(self.current_transform)
                         self.reset_pos = True
                         is_first_message = False
         except Exception as e:
@@ -50,10 +176,10 @@ class PicoJoystick:
         pass
 
     def is_reset_pos(self):
-        if self.reset_pos:
-            self.reset_pos = False
-            return True
-        return False
+        return self.reset_pos
+    
+    def set_reset_pos(self, reset_pos):
+        self.reset_pos = reset_pos
 
     def get_transform_list(self):
         with self.mutex:
@@ -93,22 +219,47 @@ class PicoJoystick:
         new_xmat = np.dot(R_yaw, np.dot(R_pitch, R_roll))
         return new_xmat
         
+
     def extact_single_transform(self, body):
         position = body["position"]
         p_x = position["x"]
         p_y = position["y"]
         p_z = position["z"]
+        pos = transform_position_to_mujoco(np.array([p_x, p_y, p_z]))
+
         rotation = body["rotation"]
         r_x = rotation["x"]
         r_y = rotation["y"]
         r_z = rotation["z"]
-        # r = self._calc_rotate_matrix(r_z, -r_x, r_y)
-        r = R.from_euler('xzy', [r_y, r_z, r_x], degrees=True)
-        return [[p_z, -p_x, p_y], r]
+        r_w = rotation["w"]
+        quat = transform_quaternion_to_mujoco([r_x, r_y, r_z, r_w])
+        
+        return [np.array([pos[0], pos[1], pos[2]]), np.array([quat[3], quat[0], quat[1], quat[2]])] # 调整wxyz顺序
+        # return [np.array([p_x, p_y, p_z]), np.array([r_w, r_x, r_y, r_z])]
+
+    # def extact_single_transform_org(self, body):
+    #     position = body["position"]
+    #     p_x = position["x"]
+    #     p_y = position["y"]
+    #     p_z = position["z"]
+    #     pos = transform_position_to_mujoco(np.array([p_x, p_y, p_z]))
+
+    #     rotation = body["rotation"]
+    #     r_x = rotation["x"]
+    #     r_y = rotation["y"]
+    #     r_z = rotation["z"]
+    #     r_w = rotation["w"]
+    #     quat = transform_quaternion_to_mujoco([r_x, r_y, r_z, r_w])
+        
+    #     # return [np.array([pos[0], pos[1], pos[2]]), np.array([quat[3], quat[0], quat[1], quat[2]])] # 调整wxyz顺序
+    #     return [np.array([p_x, p_y, p_z]), np.array([r_w, r_x, r_y, r_z])]
+
         
     def extact_all_transform(self, message):
         left_hand_transform = self.extact_single_transform(message["leftHand"])
+        # left_hand_transform_org = self.extact_single_transform_org(message["leftHand"])
         right_hand_transform = self.extact_single_transform(message["rightHand"])
+        # right_hand_transform_org = self.extact_single_transform_org(message["rightHand"])
         motion_trackers_transform = []
         if "motionTrackers" in message:
             for motionTracker in message["motionTrackers"]:
@@ -130,22 +281,35 @@ class PicoJoystick:
         return {"leftHand": left_hand_key_state, "rightHand": right_hand_key_state}
 
     def get_left_relative_move(self, transform):
-        relative_position = [x - y for x, y in zip(transform[0][0], self.first_transform[0][0])]
-        relative_rotation = self.first_transform[0][1].inv() * transform[0][1]
+        relative_position = transform[0][0]
+        relative_rotation = transform[0][1]
+        # print("rotation: ", relative_rotation.as_euler('xzy', degrees=True))
+        # relative_rotation = transform[0][1]
         # relative_rotation = np.dot(self.first_transform[0][1].T, transform[0][1])
         return [relative_position, relative_rotation]
     
+    # def get_left_relative_move_org(self, transform):
+    #     relative_position = transform[3][0]
+    #     relative_rotation = transform[3][1]
+    #     return [relative_position, relative_rotation]
+    
     def get_right_relative_move(self, transform):
-        relative_position = [x - y for x, y in zip(transform[1][0], self.first_transform[1][0])]
-        relative_rotation = self.first_transform[1][1].inv() * transform[1][1]
+        relative_position = transform[1][0]
+        relative_rotation = transform[1][1]
+        # relative_rotation = transform[1][1]
         # relative_rotation = np.dot(self.first_transform[1][1].T, transform[1][1])
         return [relative_position, relative_rotation]
+    
+    # def get_right_relative_move_org(self, transform):
+    #     relative_position = transform[4][0]
+    #     relative_rotation = transform[4][1]
+    #     return [relative_position, relative_rotation]
     
     def get_motion_trackers_relative_move(self, transform):
         motion_trackers_relative_move = []
         for i in range(len(transform[2])):
-            relative_position = [x - y for x, y in zip(transform[2][i][0], self.first_transform[2][i][0])]
-            relative_rotation = self.first_transform[2][i][1].inv() * transform[2][i][1]
+            relative_position = transform[2][i][0]
+            relative_rotation = transform[2][i][1]
             # relative_rotation = np.dot(self.first_transform[2][i][1].T, transform[2][i][1])
             motion_trackers_relative_move.append([relative_position, relative_rotation])
         return motion_trackers_relative_move