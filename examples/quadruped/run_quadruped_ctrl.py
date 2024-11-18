76e492267302889949c4f3df51160a2c314ffd31
superfhwl <superfhwl@gmail.com>
2024-11-18 13:05:26 +0800

    Update quadruped example

--- examples/quadruped/run_quadruped_ctrl.py
+++ examples/quadruped/run_quadruped_ctrl.py
@@ -8,8 +8,11 @@ if __name__ == '__main__':
     The startup script for the quadruped robot dog controller uses an MPC controller. For more details, refer to `envs/quadruped/README.md`.
     """
 
-    os.environ['LD_LIBRARY_PATH'] = os.environ.get('LD_LIBRARY_PATH', '') + "/home/superfhwl/repo/acados/lib"
-    os.environ['ACADOS_SOURCE_DIR'] = "/home/superfhwl/repo/acados"
+    current_file_path = os.path.abspath('')
+    acados_path = os.path.join(current_file_path, "../../3rd_party/acados/")
+    os.environ['LD_LIBRARY_PATH'] = os.environ.get('LD_LIBRARY_PATH', '') + acados_path + "lib"
+    os.environ['ACADOS_SOURCE_DIR'] = acados_path
+
 
     parser = argparse.ArgumentParser(description='Simulation Configuration')
     parser.add_argument('--orcagym_addr', type=str, required=True, help='The gRPC address for the simulation')
