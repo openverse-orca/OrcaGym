72bc122515e3c1c71109575890a4322de469d6db
yechaojcn <yechaojcn@aliyun.com>
2024-11-18 15:34:40 +0800

    添加机器人相关控制代码（rm75bv_joystick_env.py rm75bv_vr_env.py），修改realman controller

---
+++ envs/realman_rm65b/robot_controller.py
@@ -0,0 +1,239 @@
+import datetime
+from .robotic_arm_package.robotic_arm import *
+from .roh_registers import *
+import numpy as np
+import threading
+
+COM_PORT = 1
+ROH_ADDR = 2
+
+
+def L_BYTE(v):
+    return v & 0xFF
+
+
+def H_BYTE(v):
+    return (v >> 8) & 0xFF
+
+
+
+
+MAX_SINGLE_FINGER_POS = [H_BYTE(65535), L_BYTE(65535)]
+
+MIN_SINGLE_FINGER_POS = [H_BYTE(0), L_BYTE(0)]
+
+HAND_FIST_CLOSE_POS = [
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(65535), L_BYTE(65535),
+    H_BYTE(65535), L_BYTE(65535),
+    H_BYTE(65535), L_BYTE(65535),
+    H_BYTE(65535), L_BYTE(65535),
+    H_BYTE(65535), L_BYTE(65535),
+]
+
+HAND_FIST_OPEN_POS = [
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(65535), L_BYTE(65535),
+]
+
+HAND_VICTORY_POS = [
+    H_BYTE(65535), L_BYTE(65535),
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(0), L_BYTE(0),
+    H_BYTE(65535), L_BYTE(65535),
+    H_BYTE(65535), L_BYTE(65535),
+    H_BYTE(65535), L_BYTE(65535),
+]
+
+target_fingers = [
+    ROH_FINGER_POS_TARGET0, #拇指 弯曲
+    ROH_FINGER_POS_TARGET1, #食指
+    ROH_FINGER_POS_TARGET2, #中指
+    ROH_FINGER_POS_TARGET3, #无名指
+    ROH_FINGER_POS_TARGET4, #小指
+    ROH_FINGER_POS_TARGET5, #拇指 转动
+]
+
+
+class DeviceType:
+    RM65 = 1
+    RM75 = 2
+
+class RobotController:
+    def __init__(self,ip,devtype) -> None:
+        self.enabled = True
+        if not self.enabled:
+            return
+        
+        # self.rm65_ip= "192.168.33.80"
+       # self.rm65_ip= "192.168.1.18"
+        self.ip= ip
+       
+       # self.robot = Arm(RM65, self.rm65_ip)
+        self.robot = Arm(RM75, self.ip)
+        self.ctrlnum = devtype==DeviceType.RM65 and 6 or 7
+       
+       
+       
+        # callback = RealtimePush_Callback(RobotController.MCallback)
+        # self.robot.Realtime_Arm_Joint_State(callback)
+
+        self.mutex= threading.Lock()
+        self.ctrl = None
+
+        self.gripper_open = True
+        self.gripper_changed = False
+
+        self.running = True
+        print("RobotController init 。。。。")
+        self.thread =threading.Thread(target=self.loop)
+        self.thread.start()
+
+    def MCallback(data):
+        print("推送数据的机械臂IP: ", data.arm_ip)
+        print("机械臂错误码: ", data.arm_err)
+        print("系统错误码: ", data.sys_err)
+        print("机械臂当前角度: ", list(data.joint_status.joint_position))
+        print("机械臂当前位姿： ", data.waypoint.position.x, data.waypoint.position.y, data.waypoint.position.z,
+            data.waypoint.euler.rx, data.waypoint.euler.ry, data.waypoint.euler.rz)
+
+    
+    def __del__(self):
+        if not self.enabled:
+            return
+    
+        self.running = False
+        self.thread.join()
+        self.robot.RM_API_UnInit()
+        self.robot.Arm_Socket_Close()
+
+    def init_joint_state(self, ctrl: np.array):
+        if not self.enabled:
+            return
+        
+        #self.robot.Movej_Cmd((ctrl * 57.29577951308232).tolist()[0:6], 30, 0, 0, True)
+        self.robot.Movej_Cmd((ctrl * 57.29577951308232).tolist()[0:self.ctrlnum], 30, 0, 0, True)
+        print("init_joint_state: ", ctrl)
+
+    def sync_joint(self, ctrl: np.array):
+        if not self.enabled:
+            return
+        
+        self.mutex.acquire()
+        self.ctrl = ctrl
+        print("sync_joint.............................: ", ctrl)
+        self.mutex.release()
+
+    def loop(self):
+        while self.running:
+            do_nothing = True
+           # print("loop1111111.............................: ")
+            if self.ctrl is not None:
+                #print("loop2222.............................: ")
+                self.mutex.acquire()
+                ctrl = self.ctrl.copy()
+                self.ctrl = None
+                self.mutex.release()
+                do_nothing = False
+               # print("loop333333.............................: ", ctrl)
+                mm = ctrl * 57.29577951308232
+                #self.robot.Movej_CANFD((ctrl * 57.29577951308232).tolist()[0:6], True)
+                self.robot.Movej_CANFD((ctrl * 57.29577951308232).tolist()[0:self.ctrlnum], True)
+                print("loop.............................: ", mm)
+                # self.robot.Movej_Cmd((ctrl * 57.29577951308232).tolist()[0:6], 30, 0, 0, True)
+
+            if self.gripper_changed:
+                self.mutex.acquire()
+                self.gripper_changed = False
+                gripper_open = self.gripper_open
+                self.mutex.release()
+                do_nothing = False
+                if gripper_open:
+                    self.robot.Set_Gripper_Pick_On(speed=500, force=500)
+                    print("gripper pick")
+                else:
+                    self.robot.Set_Gripper_Release(speed=500)
+                    print("gripper release")
+
+            if do_nothing:
+                time.sleep(0.002)
+
+    def gripper_pick_on(self):
+        if not self.enabled:
+            return
+        
+        self.mutex.acquire()
+        if not self.gripper_open:
+            self.gripper_changed = True
+            self.gripper_open = True
+        self.mutex.release()
+
+    def gripper_release(self):
+        if not self.enabled:
+            return
+        
+        self.mutex.acquire()
+        if self.gripper_open:
+            self.gripper_changed = True
+            self.gripper_open = False
+        self.mutex.release()
+    
+    
+    def finger_move(self,target, num_finger, HAND_POS, delay):
+        self.robot.Write_Registers(
+            COM_PORT,
+            target,
+            num_finger,
+            HAND_POS,
+            ROH_ADDR,
+            True
+        )
+        time.sleep(delay)
+    
+    def roh_test(self):
+        print("roh_test..............................")
+        self.robot.Close_Modbustcp_Mode()
+
+        self.robot.Set_Modbus_Mode(1, 115200, 1, True)
+        tt = [0, 0, 100, 62, 150, 62, 220, 180, 50, 79, 127, 0]
+       # tt = [0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0]
+       # self.finger_move(target_fingers[0], 6, tt, 0.005)
+        tt = [0, 0, 100, 62, 150, 62, 220, 180, 0, 79, 127, 0]
+        self.finger_move(target_fingers[0], 6, tt, 2)
+        for index in range(20):
+            tt = [0, 0, 100, 62, 150, 62, 220, 180, index*12, 79, 127, 0]
+            self.finger_move(target_fingers[0], 6, tt, 0.005)
+      #  self.finger_move(target_fingers[4], 1, MAX_SINGLE_FINGER_POS, 1.0)
+       # self.finger_move(target_fingers[5], 1, MIN_SINGLE_FINGER_POS, 1.0)
+       # self.finger_move(target_fingers[0], 6, HAND_FIST_CLOSE_POS, 1.0)
+      #  for target in reversed(target_fingers):
+         #   self.finger_move(target, 1, MAX_SINGLE_FINGER_POS, 1.0)
+        '''
+
+        self.robot.Write_Registers(
+            COM_PORT,
+            ROH_FINGER_POS_TARGET0,
+            6,
+            [
+                H_BYTE(0),
+                L_BYTE(0),
+                H_BYTE(0),
+                L_BYTE(0),
+                H_BYTE(0),
+                L_BYTE(0),
+                H_BYTE(0),
+                L_BYTE(0),
+                H_BYTE(0),
+                L_BYTE(0),
+                H_BYTE(65535),
+                L_BYTE(65535),
+            ],
+            ROH_ADDR,
+            True,
+        )  # 后两位是大拇指旋转  #前两位大拇指弯曲
+    '''    
+        return True
--- examples/realman/rm75bv_vr_osc_ctrl.py
+++ examples/realman/rm75bv_vr_osc_ctrl.py
@@ -13,20 +13,25 @@ if project_root not in sys.path:
 import gymnasium as gym
 from gymnasium.envs.registration import register
 from datetime import datetime
+#from envs.orca_gym_env import ActionSpaceType
 
 # 
-TIME_STEP = 0.005
+#TIME_STEP = 0.005
+TIME_STEP = 0.01
 
 def register_env(orcagym_addr, env_name, env_index, control_freq=20) -> str:
+    print("register_env: ", orcagym_addr)
     orcagym_addr_str = orcagym_addr.replace(":", "-")
     env_id = env_name + "-OrcaGym-" + orcagym_addr_str + f"-{env_index:03d}"
     gym.register(
         id=env_id,
-        entry_point="envs.realman.rm65b_joystick_env:RM65BJoystickEnv",
+        entry_point="envs.realman.rm75bv_vr_env:RM75BVVREnv",
         kwargs={'frame_skip': 1,   
                 'reward_type': "dense",
-                'orcagym_addr': orcagym_addr, 
-                'agent_names': ['RM65B'], 
+               # 'action_space_type': ActionSpaceType.CONTINUOUS,
+              #  'action_step_count': 0,
+                'grpc_address': orcagym_addr, 
+                'agent_names': ['RM75_B_V'], 
                 'time_step': TIME_STEP,
                 'control_freq': control_freq},
         max_episode_steps=sys.maxsize,
@@ -36,28 +41,31 @@ def register_env(orcagym_addr, env_name, env_index, control_freq=20) -> str:
 
 def continue_training(env):
     observation, info = env.reset(seed=42)
+    print("""
+        To use VR controllers, please press left joystick to connect / disconnect to the simulator.
+        And then press right joystick to reset the robot's hands to the initial position.
+        """)  
     while True:
         start_time = datetime.now()
 
         action = env.action_space.sample()
         observation, reward, terminated, truncated, info = env.step(action)
 
-        # 
         elapsed_time = datetime.now() - start_time
         if elapsed_time.total_seconds() < TIME_STEP:
             time.sleep(TIME_STEP - elapsed_time.total_seconds())
-
     
 
 if __name__ == "__main__":
     try:
         orcagym_addr = "localhost:50051"
-        print("simulation running... , orcagym_addr: ", orcagym_addr)
-
-        env_name = "RM65BXboxControl-v0"
+        print("simulation running... , grpc_address: ", orcagym_addr)
+       # env_id = f"XboxControl-v0-OrcaGym-{grpc_address[-2:]}"
+                
+        env_name = "RM75VRControl-v0"
         env_index = 0
         env_id = register_env(orcagym_addr, env_name, env_index, 20)
-        print("Registering environment with id: ", env_id)
+       # register_env(grpc_address, 20)
 
         env = gym.make(env_id)        
         print("Start Simulation!")
