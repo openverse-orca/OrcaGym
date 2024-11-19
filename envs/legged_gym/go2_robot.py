76c7e9b1e90c86347f02e33f9a61ce16c950c268
superfhwl <superfhwl@gmail.com>
2024-11-19 16:54:07 +0800

    Update legged gym & robot

--- envs/legged_gym/go2_robot.py
+++ envs/legged_gym/go2_robot.py
@@ -18,15 +18,49 @@ class Go2Robot(LeggedRobot):
                                                       "FR_hip_joint", "FR_thigh_joint", "FR_calf_joint",
                                                       "RL_hip_joint", "RL_thigh_joint", "RL_calf_joint",
                                                       "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint"])
+        self._base_joint_name = self.name_space("base")
+        self._joint_names = [self._base_joint_name] + self._leg_joint_names
         
         self._neutral_joint_angles = {"FL_hip_joint": 0.0, "FL_thigh_joint": 0.8, "FL_calf_joint": -1.2,
                                       "FR_hip_joint": 0.0, "FR_thigh_joint": 0.8, "FR_calf_joint": -1.2,
                                       "RL_hip_joint": 0.0, "RL_thigh_joint": 0.8, "RL_calf_joint": -1.2,
                                       "RR_hip_joint": 0.0, "RR_thigh_joint": 0.8, "RR_calf_joint": -1.2}
         
-        self._leg_actuator_names = self.name_space_list(["FR_hip", "FR_thigh", "FR_calf",
-                                                        "FL_hip", "FL_thigh", "FL_calf",
-                                                        "RR_hip", "RR_thigh", "RR_calf",
-                                                        "RL_hip", "RL_thigh", "RL_calf"])
+        self._actuator_names = self.name_space_list(["FR_hip", "FR_thigh", "FR_calf",
+                                                    "FL_hip", "FL_thigh", "FL_calf",
+                                                    "RR_hip", "RR_thigh", "RR_calf",
+                                                    "RL_hip", "RL_thigh", "RL_calf"])
         
-        self._ctrl = np.zeros(len(self._leg_actuator_names))
+        self._imu_site_name = self.name_space("imu")
+        self._contact_site_names = self.name_space_list(["base_contact_box", "base_contact_cylinder", "base_contact_sphere"])
+        self._site_names = [self._imu_site_name] + self._contact_site_names
+
+        self._sensor_imu_quat_name = self.name_space("imu_quat")
+        self._sensor_imu_omega_name = self.name_space("imu_omega")
+        self._sensor_imu_acc_name = self.name_space("imu_acc")
+        self._sensor_base_touch_box_name = self.name_space("base_touch_box")
+        self._sensor_base_touch_cylinder_name = self.name_space("base_touch_cylinder")
+        self._sensor_base_touch_sphere_name = self.name_space("base_touch_sphere")
+        self._sensor_names = [self._sensor_imu_quat_name, self._sensor_imu_omega_name, self._sensor_imu_acc_name,
+                              self._sensor_base_touch_box_name, self._sensor_base_touch_cylinder_name, self._sensor_base_touch_sphere_name]
+        self._touch_sensor_names = [self._sensor_base_touch_box_name, self._sensor_base_touch_cylinder_name, self._sensor_base_touch_sphere_name]
+
+        self._contact_force_threshold = np.array([0.5, 0.5, 0.5])
+        
+        self._ctrl = np.zeros(len(self._actuator_names))
+        self._nu = len(self._actuator_names)
+        self._nq = len(self._leg_joint_names) + (7 * len(self._base_joint_name))
+        self._nv = len(self._leg_joint_names) + (6 * len(self._base_joint_name))
+
+    def get_body_contact_force(self, sensor_data : dict) -> np.ndarray:
+        contact_force = np.zeros(len(self._touch_sensor_names))
+        for i, sensor_name in enumerate(self._touch_sensor_names):
+            contact_force[i] = sensor_data[sensor_name]
+        return contact_force.flatten()
+    
+    def get_imu_data(self, sensor_data: dict) -> np.ndarray:
+        quat = np.array([sensor_data[self._sensor_imu_quat_name]])
+        omega = np.array([sensor_data[self._sensor_imu_omega_name]])
+        acc = np.array([sensor_data[self._sensor_imu_acc_name]])
+        imu_data = np.concatenate([quat, omega, acc], axis=1)
+        return imu_data.flatten()
--- envs/legged_gym/legged_gym_env.py
+++ envs/legged_gym/legged_gym_env.py
@@ -23,11 +23,6 @@ class LeggedGymEnv(OrcaGymLocalEnv):
         render_remote: bool,
         env_id: str,
         task: str,
-        distance_threshold: float,
-        goal_xy_range: float,
-        obj_xy_range: float,
-        goal_x_offset: float,
-        goal_z_range: float,
         **kwargs,
     ):
 
@@ -44,20 +39,16 @@ class LeggedGymEnv(OrcaGymLocalEnv):
         )
 
         self._task = task
-        self._distance_threshold = distance_threshold
 
         self._agents: list[LeggedRobot] = []
         for agent_name in agent_names:
-            agent = LeggedRobot(agent_name, task, max_episode_steps, distance_threshold, 
-                                goal_xy_range, obj_xy_range, goal_x_offset, goal_z_range)
+            agent = LeggedRobot(agent_name, task, max_episode_steps)
             self._agents.append(agent)
 
-        self._agent_ee_names = [agent.ee_name for agent in self._agents]
-        self._agent_obj_joint_names = [agent.obj_joint_name for agent in self._agents]
-        self._agent_obj_site_names = [agent.obj_site_name for agent in self._agents]
-        self._agent_mocap_names = [agent.mocap_name for agent in self._agents]
-        self._agent_gripper_joint_names = [gripper_joint_name for agent in self._agents for gripper_joint_name in agent.gripper_joint_names]
-        self._agent_joint_names = [joint_name for agent in self._agents for joint_name in agent.joint_names]
+        self._agent_joint_names = [agent.joint_names for agent in self._agents]
+        self._agent_actuator_names = [agent.actuator_names for agent in self._agents]
+        self._agent_site_names = [agent.site_names for agent in self._agents]
+        self._agent_sensor_names = [agent.sensor_names for agent in self._agents]
 
         self.nu = self.model.nu
         self.nq = self.model.nq
@@ -65,15 +56,9 @@ class LeggedGymEnv(OrcaGymLocalEnv):
 
         # control range
         all_actuator = self.model.get_actuator_dict()
-        [agent.set_gripper_ctrl_range(all_actuator) for agent in self._agents]
+        [agent.set_acutator_ctrl_range(all_actuator) for agent in self._agents]
 
         self.ctrl = np.zeros(self.nu)
-        [agent.set_init_obj_state(self.query_joint_qpos([agent.obj_joint_name])[agent.obj_joint_name], 
-                                    self.query_site_pos_and_mat([agent.obj_site_name])[agent.obj_site_name]['xpos'])
-                                    for agent in self._agents]
-        self._set_init_state()
-
-        [agent.set_initial_grasp_site_xpos(self.query_site_pos_and_quat([agent.ee_name])) for agent in self._agents]
 
 
         self._reset_agents(self._agents)
@@ -94,21 +79,6 @@ class LeggedGymEnv(OrcaGymLocalEnv):
             dtype=np.float32,
         )
         [agent.set_action_space(self.action_space) for agent in self._agents]
-
-    def _set_init_state(self) -> None:
-        # print("Set initial state")
-        self._set_joint_neutral(self._agents)
-
-        self._update_ctrl()
-
-        self._reset_mocap_welds()
-
-        self.mj_forward()
-
-    def _update_ctrl(self) -> None:
-        joint_qpos = self.query_joint_qpos(self._agent_joint_names)
-        ctrl = np.array([joint_qpos[joint_name] for joint_name in self._agent_joint_names]).flat.copy()
-        self.ctrl[:len(ctrl)] = ctrl
         
 
     def step(self, action) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
@@ -120,18 +90,10 @@ class LeggedGymEnv(OrcaGymLocalEnv):
         action = np.array(action).reshape(len(self._agents), -1)
 
         # step 每个 agent，输入 ee_pos_quat，计算 mocap 和 gripper 控制
-        ee_pos_quat = self.query_site_pos_and_quat(self._agent_ee_names)
-        grasp_mocap, gripper_qpos = {}, {}
-        for agent, act in zip(self._agents, action):
-            agent_grasp_mocap, agent_gripper_qpos = agent.step(act, ee_pos_quat)
-            grasp_mocap.update(agent_grasp_mocap)
-            gripper_qpos.update(agent_gripper_qpos)
-
-        self.set_mocap_pos_and_quat(grasp_mocap)
-        self.set_joint_qpos(gripper_qpos)
-        self.mj_forward()
-
-        self._update_ctrl()
+        for i in range(len(self._agents)):
+            agent = self._agents[i]
+            act = action[i]
+            self.ctrl[i : i + len(act)] = agent.step(act)
 
         self.do_simulation(self.ctrl, self.frame_skip)
 
@@ -148,11 +110,11 @@ class LeggedGymEnv(OrcaGymLocalEnv):
         truncated = [False for _ in range(len(self._agents))]
         agents_to_reset : list[LeggedRobot] = []
         for i, agent in enumerate(self._agents):
-            info["is_success"][i] = agent.is_success(obs["achieved_goal"][i * achieved_goal_shape : (i + 1) * achieved_goal_shape], 
-                                                    obs["desired_goal"][i * desired_goal_shape : (i + 1) * desired_goal_shape], self._env_id)
-            reward[i] = agent.compute_reward(obs["achieved_goal"][i * achieved_goal_shape : (i + 1) * achieved_goal_shape],
-                                                obs["desired_goal"][i * desired_goal_shape : (i + 1) * desired_goal_shape])
-            terminated[i] = bool(info["is_success"][i] != 0)
+            achieved_goal = obs["achieved_goal"][i * achieved_goal_shape : (i + 1) * achieved_goal_shape]
+            desired_goal = obs["desired_goal"][i * desired_goal_shape : (i + 1) * desired_goal_shape]
+            info["is_success"][i] = agent.is_success(achieved_goal, desired_goal, self._env_id)
+            reward[i] = agent.compute_reward(achieved_goal, desired_goal)
+            terminated[i] = agent.is_terminated(achieved_goal, desired_goal)
             truncated[i] = agent.truncated
 
             if (terminated[i] or truncated[i]):
@@ -172,19 +134,26 @@ class LeggedGymEnv(OrcaGymLocalEnv):
 
 
     def compute_reward(self, achieved_goal, desired_goal, info) -> SupportsFloat:
-        # print("Compute reward : ", len(achieved_goal), len(desired_goal))
-        return self._agents[0].compute_reward(achieved_goal, desired_goal)
+        assert achieved_goal.shape == desired_goal.shape
+        if achieved_goal.ndim == 1:
+            return self._agents[0].compute_reward(achieved_goal, desired_goal)
+        elif achieved_goal.ndim == 2:
+            agent_num = len(self._agents)
+            rewards = np.zeros(agent_num)
+            for i in range(len(achieved_goal)):
+                rewards[i] = self._agents[i % agent_num].compute_reward(achieved_goal[i], desired_goal[i])
+            return rewards
+        else:
+            raise ValueError("Unsupported achieved_goal shape")
 
     def _get_obs(self, agents : list[LeggedRobot]) -> list[dict]:
-        site_pos_quat = self.query_site_pos_and_quat(self._agent_ee_names)
-        site_pos_mat = self.query_site_pos_and_mat(self._agent_obj_site_names)
-        site_xvalp, site_xvalr = self.query_site_xvalp_xvalr(self._agent_obj_site_names)
-        joint_qpos = self.query_joint_qpos(self._agent_gripper_joint_names)
+        joint_qpos = self.query_joint_qpos(self._agent_joint_names)
+        sensor_data = self.query_sensor_data(self._agent_sensor_names)
 
-        # 这里，每个process将多个agent的obs拼接在一起，在 subproc_vec_env 再展开成mxn份
-        obs = agents[0].get_obs(site_pos_quat, site_pos_mat, site_xvalp, site_xvalr, joint_qpos, self.dt)
+        # 这里，每个process将多个agent的obs拼接在一起，在 subproc_vec_env 再展开成 m x n 份
+        obs = agents[0].get_obs(sensor_data, joint_qpos, self.dt)
         for i in range(1, len(agents)):
-            agent_obs = agents[i].get_obs(site_pos_quat, site_pos_mat, site_xvalp, site_xvalr, joint_qpos, self.dt)
+            agent_obs = agents[i].get_obs(sensor_data, joint_qpos, self.dt)
             obs = {key: np.concatenate([obs[key], agent_obs[key]]) for key in obs.keys()}
         
         return obs
@@ -193,13 +162,11 @@ class LeggedGymEnv(OrcaGymLocalEnv):
         if len(agents) == 0:
             return
 
-        mocap_pos_quat, joint_qpos = {}, {}
+        joint_qpos = {}
         for agent in agents:
-            agent_mocap, agent_joint_qpos = agent.reset(self.np_random)
-            mocap_pos_quat.update(agent_mocap)
+            agent_joint_qpos = agent.reset(self.np_random)
             joint_qpos.update(agent_joint_qpos)
 
-        self.set_mocap_pos_and_quat(mocap_pos_quat)
         self.set_joint_qpos(joint_qpos)
         self.mj_forward()
 
@@ -208,36 +175,9 @@ class LeggedGymEnv(OrcaGymLocalEnv):
         print("Reset model")
 
         # 依次 reset 每个agent
-        self._set_init_state()
         self._reset_agents(self._agents)
         obs = self._get_obs(self._agents).copy()
         return obs
-
-    # custom methods
-    # -----------------------------
-    def _reset_mocap_welds(self) -> None:
-        if self.model.nmocap > 0 and self.model.neq > 0:
-            eq_list = self.model.get_eq_list()
-            for eq in eq_list:
-                if eq['eq_type'] == self.model.mjEQ_WELD:
-                    obj1_id = eq['obj1_id']
-                    obj2_id = eq['obj2_id']
-                    eq_data = eq['eq_data'].copy()
-                    # print("org eq_data: ", eq_data)
-                    eq_data[3:10] = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
-                    self.update_equality_constraints([{"obj1_id": obj1_id, "obj2_id": obj2_id, "eq_data": eq_data}])
-        self.mj_forward()
-
-    def _set_goal_mocap(self, goal_name, position, orientation) -> None:
-        mocap_pos_and_quat_dict = {goal_name: {'pos': position, 'quat': orientation}}
-        self.set_mocap_pos_and_quat(mocap_pos_and_quat_dict)
-
-    def _set_joint_neutral(self, agents : list[LeggedRobot]) -> None:
-        joint_qpos = {}
-        for agent in agents:
-            joint_qpos.update(agent.get_joint_neutral())
-
-        self.set_joint_qpos(joint_qpos)
 
 
     def get_observation(self, obs=None):
