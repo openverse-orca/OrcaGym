7c4a5c268377670b6ccc05a869a7cfbef1b09ccb
Guojiatao <ttt1570601232@163.com>
2024-11-26 14:38:09 +0800

    add h5 record to vr control

--- examples/openloong/Openloong_arm_vr_osc_ctrl.py
+++ examples/openloong/Openloong_arm_vr_osc_ctrl.py
@@ -2,12 +2,16 @@ import os
 import sys
 import time
 
+import numpy as np
+
+
+
 current_file_path = os.path.abspath('./../../')
 
 if current_file_path not in sys.path:
     print("add path: ", current_file_path)
     sys.path.append(current_file_path)
-
+from envs.robomimic.dataset_util import DatasetWriter
 
 import gymnasium as gym
 from gymnasium.envs.registration import register
@@ -44,7 +48,21 @@ def continue_training(env):
 
         action = env.action_space.sample()
         observation, reward, terminated, truncated, info = env.step(action)
+        # Collect data after each step
+        obs_list = [observation]  # Collect observations for this step
+        action_list = [action]  # Collect actions
+        reward_list = [reward]  # Collect rewards
+        done_list = [1 if terminated else 0]  # Collect done flags
+        info_list = [info]  # Collect additional info (like state)
 
+        # Save data to .h5 using DatasetWriter
+        dataset_writer.add_demo({
+            'states': np.array([np.concatenate([info["state"]["qpos"], info["state"]["qvel"]]) for info in info_list]),
+            'actions': np.array(action_list),
+            'rewards': np.array(reward_list),
+            'dones': np.array(done_list),
+            'obs': obs_list
+        })
         elapsed_time = datetime.now() - start_time
         if elapsed_time.total_seconds() < TIME_STEP:
             time.sleep(TIME_STEP - elapsed_time.total_seconds())
@@ -63,7 +81,12 @@ if __name__ == "__main__":
         env_index = 0
         env_id = register_env(orcagym_addr, env_name, env_index, 20)
         print("Registering environment with id: ", env_id)
-
+        # Create a new DatasetWriter instance to save the data
+        formatted_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        dataset_writer = DatasetWriter(file_path=f"teleoperation_dataset_{formatted_now}.hdf5",
+                                       env_name=env_name,
+                                       env_version="v1",
+                                       env_kwargs={'orcagym_addr': orcagym_addr})
         env = gym.make(env_id)        
         print("Starting simulation...")
 
