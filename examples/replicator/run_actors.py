fix: Update rotation for cart_basket actor and enable material info setting in scene

examples/replicator/run_actors.py
@@ -47,7 +47,7 @@ def create_scene() -> OrcaGymScene:
         name="cart_basket",
         spawnable_name="cart_basket_usda",
         position=np.array([0, 0, 0.0]),
-        rotation=rotations.euler2quat(np.array([0.0, 0.0, np.random.uniform(-np.pi, np.pi)])),
+        rotation=rotations.euler2quat(np.array([0.0, 0.0, 0.0])),
         scale=1.0,
     )
     scene.add_actor(actor)
@@ -73,15 +73,15 @@ def create_scene() -> OrcaGymScene:
     scene.publish_scene()
 
     # scene.make_camera_viewport_active("default_camera", "CameraViewport")
-    # for i in range(10):
-    #     material_info = MaterialInfo(
-    #         base_color=np.array([
-    #             np.random.uniform(0.0, 1.0),
-    #             np.random.uniform(0.0, 1.0),
-    #             np.random.uniform(0.0, 1.0),
-    #             1.0]),
-    #     )
-    #     scene.set_material_info(f"cup_with_random_color_and_scale_{i}", material_info)
+    for i in range(10):
+        material_info = MaterialInfo(
+            base_color=np.array([
+                np.random.uniform(0.0, 1.0),
+                np.random.uniform(0.0, 1.0),
+                np.random.uniform(0.0, 1.0),
+                1.0]),
+        )
+        scene.set_material_info(f"cup_with_random_color_and_scale_{i}", material_info)
 
 
     print("Replicator scene published successfully.")
@@ -169,9 +169,9 @@ if __name__ == "__main__":
 
     orcagym_addr = "localhost:50051"
     agent_name = "NoRobot"
-    # run_simulation(orcagym_addr, agent_name)
+    run_simulation(orcagym_addr, agent_name)
 
-    time.sleep(10)
+    # time.sleep(3)
 
     destroy_scene(scene)
 