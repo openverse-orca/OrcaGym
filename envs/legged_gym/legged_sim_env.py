5f907ad39844b58fe1c0cadb1e9b280d2eba75f2
Yao <yaoxiangfd@gmail.com>
2025-06-05 18:20:20 +0800

    cp

--- envs/legged_gym/legged_sim_env.py
+++ envs/legged_gym/legged_sim_env.py
@@ -311,7 +311,7 @@ class AgentBase:
         )
         
         action_size = self.agent.get_action_size()
-        self._action_range = np.array([[-2.0, 2.0]] * action_size, dtype=np.float32)
+        self._action_range = np.array([[-3.0, 3.0]] * action_size, dtype=np.float32)
         action_space = spaces.Box(
             low=self._action_range[:, 0],
             high=self._action_range[:, 1],
@@ -429,6 +429,11 @@ class AgentBase:
     def _query_ctrl_info(self):
         ctrl_info = {}
         ctrl_info[self.agent.name] = self.agent.get_ctrl_info()
+        # # store ctrl_info to pkl file, whichn is used for 2real; commented otherwise
+        # import pickle
+        # with open(f"ctrl_info.pkl", "wb") as f:
+        #     pickle.dump(ctrl_info, f)
+        # exit()
         return ctrl_info    
     
     def _init_playable(self, env: LeggedSimEnv) -> None:
