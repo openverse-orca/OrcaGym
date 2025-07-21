c10f8f401ec3c5e8d4766b0612bf593bfbf6ce6d
superfhwl <superfhwl@gmail.com>
2025-07-21 11:49:27 +0800

    Refactor character control to support waypoint navigation and enhance keyboard input handling. Updated character configuration to include acceleration and waypoint parameters. Removed debug print statements and improved movement logic for smoother transitions.

--- envs/character/character.py
+++ envs/character/character.py
@@ -3,6 +3,7 @@ import yaml
 from orca_gym.devices.keyboard import KeyboardInput, KeyboardInputSourceType
 import os
 import numpy as np
+import time
 
 class Character():
     def __init__(self, 
@@ -50,11 +51,22 @@ class Character():
         }
 
         self._speed = self._config['speed']
+        self._acceleration = self._speed['Acceleration']
         self._move_speed = 0.0
         self._turn_speed = 0.0
 
-        self._use_keyboard_control = self._config['use_keyboard_control']
+        self._use_keyboard_control = self._config['control_type'] == "keyboard"
         self._keyboard_control = self._config['keyboard_control']
+
+
+        self._use_waypoint_control = self._config['control_type'] == "waypoint"
+        self._waypoint_control = self._config['waypoint_control']
+        self._waypoint_distance_threshold = self._config['waypoint_distance_threshold']
+        self._waypoint_angle_threshold = self._config['waypoint_angle_threshold']
+
+        body_xpos, _, _ = self._env.get_body_xpos_xmat_xquat([self._body_name])
+        self._original_coordinates = body_xpos[:2]
+        print("Original Coordinates: ", self._original_coordinates)
 
         
     def on_step(self):
@@ -63,12 +75,18 @@ class Character():
 
         if self._use_keyboard_control:
             self._process_keyboard_input(heading)
+        elif self._use_waypoint_control:
+            self._process_waypoint_input(heading)
 
         self._env.set_joint_qvel(self._ctrl_joint_qvel)
 
     def on_reset(self):
         self._move_speed = 0
         self._turn_speed = 0
+        self._waypoint_time = 0
+        self._waypoint_index = -1
+        self._moving_to_waypoint = False
+        self._next_waypoint_coord = None
         self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Forward", False)
         self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Backward", False)
         self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnLeft", False)
@@ -79,6 +97,48 @@ class Character():
         }
 
         self._env.set_joint_qpos(ctrl_qpos)
+
+
+    def _turn_left(self):
+        self._turn_speed = self._speed['TurnLeft']
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnLeft", True)
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnRight", False)
+
+    def _turn_right(self):
+        self._turn_speed = self._speed['TurnRight']
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnRight", True)
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnLeft", False)
+    
+
+    def _stop_turning(self):
+        self._turn_speed = 0
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnLeft", False)
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnRight", False)
+
+    def _move_forward(self):
+        if self._move_speed < self._speed['Forward']:
+            self._move_speed += self._speed['Forward'] * self._acceleration
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Forward", True)
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Backward", False)
+
+    def _move_backward(self):
+        if self._move_speed > self._speed['Backward']:
+            self._move_speed += self._speed['Backward'] * self._acceleration
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Backward", True)
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Forward", False)
+
+    def _stop_moving(self):
+        self._move_speed = 0
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Forward", False)
+        self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Backward", False)
+
+    def _process_move(self, move_speed : float, heading : float):
+        move_y_vel = move_speed * np.cos(heading)
+        move_x_vel = move_speed * -np.sin(heading)
+
+        self._ctrl_joint_qvel[self._joint_names["Move_X"]][0] = move_x_vel
+        self._ctrl_joint_qvel[self._joint_names["Move_Y"]][0] = move_y_vel
+        self._ctrl_joint_qvel[self._joint_names["Rotate_Z"]][0] = self._turn_speed
 
     def _process_keyboard_input(self, heading : float):
         if not self._use_keyboard_control:
@@ -91,37 +151,71 @@ class Character():
         # print("Speed: ", self._speed)
 
         if keyboard_state[self._keyboard_control['move_forward']] == 1:
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Forward", True)
-            if self._move_speed < self._speed['Forward']:
-                self._move_speed += self._speed['Forward'] * 0.1
+            self._move_forward()
         elif keyboard_state[self._keyboard_control['move_backward']] == 1:
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Backward", True)
-            if self._move_speed > self._speed['Backward']:
-                self._move_speed += self._speed['Backward'] * 0.1
+            self._move_backward()
         else:
-            self._move_speed = 0
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Forward", False)
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "Backward", False)
-
+            self._stop_moving()
 
         if keyboard_state[self._keyboard_control['turn_left']] == 1:
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnLeft", True)
-            self._turn_speed = self._speed['TurnLeft']
+            self._turn_left()
         elif keyboard_state[self._keyboard_control['turn_right']] == 1:
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnRight", True)
-            self._turn_speed = self._speed['TurnRight']
+            self._turn_right()
         else:
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnLeft", False)
-            self._env.scene_runtime.set_actor_anim_param_bool(self._agent_name, "TurnRight", False)
-            self._turn_speed = 0
+            self._stop_turning()
 
         # print("heading: ", heading, "move_speed: ", self._move_speed, "turn_speed: ", self._turn_speed)
-        move_y_vel = self._move_speed * np.cos(heading)
-        move_x_vel = self._move_speed * -np.sin(heading)
+        self._process_move(self._move_speed, heading)
 
-        self._ctrl_joint_qvel[self._joint_names["Move_X"]][0] = move_x_vel
-        self._ctrl_joint_qvel[self._joint_names["Move_Y"]][0] = move_y_vel
-        self._ctrl_joint_qvel[self._joint_names["Rotate_Z"]][0] = self._turn_speed
+    def _process_waypoint_input(self, heading : float):
+        if not self._use_waypoint_control:
+            return
+
+        current_time = time.time()
+        time_delta = current_time - self._waypoint_time
+
+        if not self._moving_to_waypoint and time_delta > self._waypoint_control[self._waypoint_index]['Duration']:
+            # time to go to next waypoint
+            self._moving_to_waypoint = True
+            self._waypoint_index += 1
+            if self._waypoint_index >= len(self._waypoint_control):
+                self._waypoint_index = 0
+            
+            next_waypoint_coord = np.array(self._waypoint_control[self._waypoint_index]['Coordinates']) + self._original_coordinates
+            print("Next Waypoint Coordinates: ", next_waypoint_coord)
+            self._next_waypoint_coord = next_waypoint_coord
+
+        if self._next_waypoint_coord is not None and self._moving_to_waypoint:
+            body_xpos, _, _ = self._env.get_body_xpos_xmat_xquat([self._body_name])
+            current_coordinates = body_xpos[:2] 
+
+            # calculate the distance to the next waypoint, and the direction
+            distance = np.linalg.norm(self._next_waypoint_coord - current_coordinates)
+            direction = (self._next_waypoint_coord - current_coordinates) / distance
+            # print("Distance: ", distance, "Direction: ", direction)
+                
+            # check if the character has reached the waypoint
+            if distance < self._waypoint_distance_threshold:
+                # reached the waypoint
+                self._stop_moving()
+                self._stop_turning()
+                self._moving_to_waypoint = False
+                self._waypoint_time = current_time
+                self._process_move(self._move_speed, heading)
+                return
+
+            # check the direction of the character, if not facing the waypoint, turn to the waypoint
+            angle_error = np.dot(direction, [np.cos(heading), np.sin(heading)])
+            if angle_error < -self._waypoint_angle_threshold:
+                self._turn_left()
+            elif angle_error > self._waypoint_angle_threshold:
+                self._turn_right()
+            else:
+                self._stop_turning()
+                self._move_forward()
+                
+            self._process_move(self._move_speed, heading)
+
 
 
             
--- examples/character/README.md
+++ examples/character/README.md
@@ -1,2 +1,13 @@
-1. 在关卡中添加 remy.prefab (引擎已经内置，直接联想就可以找到)
-2. 运行 run_character.py
+## 在关卡中添加 Remy.prefab 
+   - 引擎已经内置，在实例化预制体菜单项，直接联想就可以找到
+   - 拖动 Remy. 到需要的位置
+   - 如果更改了 Remy 的名称，需要修改 run_character.py 中的 agent_name
+
+## 运行 run_character.py
+   - 配置文件在 envs/character/character_config/remy.yaml
+   - 配置文件中可以修改 control_type 更换控制方式，控制方式有：
+     - keyboard: 键盘控制
+        - A/W/S/D 控制角色移动，也可以配置为其他按键
+     - waypoint: 路径点控制 （默认）
+        - 可以修改路径点，路径点在 envs/character/character_config/remy.yaml 中，坐标为相对角色初始位置的坐标
+        - 可以修改路径点之间的距离阈值，路径点之间的角度阈值，路径点站立时等待时间
